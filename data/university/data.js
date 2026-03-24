9913624658108487524e6822e45f6a7039ac74b7
kieuphat159 <kieuphat159@gmail.com>
2026-03-24 20:37:57 +0700

    initial

---
+++ data/university/data.js
@@ -0,0 +1,148 @@
+const universities = [
+  {
+    truong: "ĐH Công nghệ Thông tin – ĐHQG HCM (UIT)",
+    namThanhLap: 2006,
+    chuyenNganh: "Công nghệ thông tin",
+    soLuong: 8000,
+    lat: 10.869989,
+    long: 106.803057,
+    img: "./img/uit.json"
+  },
+  {
+    truong: "ĐH Bách Khoa – ĐHQG HCM",
+    namThanhLap: 1957,
+    chuyenNganh: "Kỹ thuật – Công nghệ",
+    soLuong: 35000,
+    lat: 10.771907,
+    long: 106.658202,
+    img: "./img/bachkhoa.json"
+  },
+  {
+    truong: "ĐH Khoa học Tự nhiên – ĐHQG HCM",
+    namThanhLap: 1996,
+    chuyenNganh: "Khoa học máy tính",
+    soLuong: 30000,
+    lat: 10.762615,
+    long: 106.682087,
+    img: "./img/hcmus.json"
+  },
+  {
+    truong: "ĐH Kinh tế TP.HCM",
+    namThanhLap: 1976,
+    chuyenNganh: "Kinh tế – Quản trị",
+    soLuong: 36000,
+    lat: 10.783036,
+    long: 106.694771,
+    img: "./img/ueh.json"
+  },
+  {
+    truong: "ĐH Sư phạm Kỹ thuật TP.HCM",
+    namThanhLap: 1962,
+    chuyenNganh: "Kỹ thuật – Sư phạm kỹ thuật",
+    soLuong: 28000,
+    lat: 10.850639,
+    long: 106.771918,
+    img: "./img/hcmute.json"
+  },
+  {
+    truong: "ĐH Cần Thơ",
+    namThanhLap: 1966,
+    chuyenNganh: "Nông nghiệp – Thủy sản",
+    soLuong: 45000,
+    lat: 10.029944,
+    long: 105.770643,
+    img: "./img/ctu.json"
+  },
+  {
+    truong: "ĐH Y Dược Cần Thơ",
+    namThanhLap: 2002,
+    chuyenNganh: "Y khoa",
+    soLuong: 12000,
+    lat: 10.072045,
+    long: 105.751048,
+    img: "./img/ctump.json"
+  },
+  {
+    truong: "ĐH Nam Cần Thơ",
+    namThanhLap: 2013,
+    chuyenNganh: "Công nghệ – Kinh tế",
+    soLuong: 20000,
+    lat: 10.008199,
+    long: 105.723716,
+    img: "./img/dnc.json"
+  },
+  {
+    truong: "ĐH Tây Đô",
+    namThanhLap: 2006,
+    chuyenNganh: "Công nghệ thông tin",
+    soLuong: 10000,
+    lat: 9.999065,
+    long: 105.759952,
+    img: "./img/taydo.json"
+  },
+  {
+    truong: "ĐH An Giang – ĐHQG HCM",
+    namThanhLap: 1999,
+    chuyenNganh: "Sư phạm",
+    soLuong: 15000,
+    lat: 10.371544,
+    long: 105.43236,
+    img: "./img/agu.json"
+  },
+  {
+    truong: "ĐH Đồng Tháp",
+    namThanhLap: 2003,
+    chuyenNganh: "Sư phạm",
+    soLuong: 12000,
+    lat: 10.419721,
+    long: 105.643811,
+    img: "./img/dthu.json"
+  },
+  {
+    truong: "ĐH Trà Vinh",
+    namThanhLap: 2006,
+    chuyenNganh: "Công nghệ – Kinh tế",
+    soLuong: 20000,
+    lat: 9.923349,
+    long: 106.34653,
+    img: "./img/tvu.json"
+  },
+  {
+    truong: "ĐH Kiên Giang",
+    namThanhLap: 2014,
+    chuyenNganh: "Nông nghiệp",
+    soLuong: 8000,
+    lat: 9.916377,
+    long: 105.144264,
+    img: "./img/kgu.json"
+  },
+  {
+    truong: "ĐH Cửu Long",
+    namThanhLap: 2000,
+    chuyenNganh: "Quản trị kinh doanh",
+    soLuong: 18000,
+    lat: 10.169624,
+    long: 105.927644,
+    img: "./img/mku.json"
+  },
+  {
+    truong: "ĐH Tiền Giang",
+    namThanhLap: 2005,
+    chuyenNganh: "Công nghệ – Sư phạm",
+    soLuong: 10000,
+    lat: 10.424275,
+    long: 106.332693,
+    img: "./img/tgu.json"
+  },
+  {
+    truong: "ĐH Bạc Liêu",
+    namThanhLap: 2006,
+    chuyenNganh: "Nuôi trồng thủy sản",
+    soLuong: 6000,
+    lat: 9.291848,
+    long: 105.712907,
+    img: "./img/blu.json"
+  }
+];
+
+export default universities;
